d0efa76061750f849e4e34542d15b69e2faf8186
Max Chen <maxchen@kfsyscc.org>
2018-08-15 17:51:35 +0800

    fix duplicate model issue

--- app/adapter_mongoose.js
+++ app/adapter_mongoose.js
@@ -5,29 +5,27 @@ const database = process.env.MONGODB_DATABASE || "test";
 
 class MongooseAdapter {
     constructor() {
-        this.dbs = {};
+        this.models = {};
         mongoose.connect(`${url}/${database}`, {
-          useNewUrlParser:true,
-          server: {
-            socketOptions: {
-              socketTimeoutMS: 0,
-              connectTimeoutMS: 0,
-              connectionTimeout: 0
-            }
-          }
+            useNewUrlParser: true,
+            socketTimeoutMS: 0,
+            connectTimeoutMS: 0
         });
     }
 
-    getModel(col_name){
-        const Any = new Schema({_id: { type: String }},{strict:false});
-        return mongoose.model(col_name, Any, col_name);
+    getModel(col_name) {
+        if(!this.models[col_name]){
+            const Any = new Schema({_id: {type: String}}, {strict: false});
+            this.models[col_name] = mongoose.model(col_name, Any, col_name);
+        }
+        return this.models[col_name];
     }
 
     doPost(col_name, data) {
         return new Promise((resolve, reject) => {
             let Model = this.getModel(col_name);
             let model = new Model(data);
-            model.save(function(err) {
+            model.save(function (err) {
                 if (err) reject(err);
                 resolve(data);
             });
@@ -37,7 +35,7 @@ class MongooseAdapter {
     doGet(col_name, id) {
         return new Promise((resolve, reject) => {
             let Model = this.getModel(col_name);
-            Model.findById(id, function(err, item) {
+            Model.findById(id, function (err, item) {
                 if (err) reject(err);
                 resolve(item);
             });
@@ -47,7 +45,7 @@ class MongooseAdapter {
     doPut(db_name, col_name, id, data) {
         return new Promise((resolve, reject) => {
             let Model = this.getModel(col_name);
-            Model.findByIdAndUpdate(id, data, function(err, item) {
+            Model.findByIdAndUpdate(id, data, function (err, item) {
                 if (err) reject(err);
                 resolve(item);
             });
@@ -58,9 +56,9 @@ class MongooseAdapter {
     doDelete(col_name, id) {
         return new Promise((resolve, reject) => {
             let Model = this.getModel(col_name);
-            Model.findById(id, function(err, item) {
+            Model.findById(id, function (err, item) {
                 if (err) reject(err);
-                item.remove(function(err) {
+                item.remove(function (err) {
                     if (err) reject(err);
                     resolve(item);
                 });
@@ -71,7 +69,7 @@ class MongooseAdapter {
     doGetList(col_name) {
         return new Promise((resolve, reject) => {
             let Model = this.getModel(col_name);
-            Model.find({}, function(err, items) {
+            Model.find({}, function (err, items) {
                 if (err) reject(err);
                 resolve(items);
             });
